72bf251df6c29b4dafe1f2bb5baa752bbde3f05b
Michelle Wegenstein <mwegenstein@gmail.com>
2014-05-14 17:46:04 -0700

    created hide or show for click event after 3 seconds. changed sidebar color to draw attention to it.

--- test.js
+++ test.js
@@ -1,9 +1,39 @@
 
+
 // Make a new link called "Blue" in the nav that, on hover, turns all the links on the page blue except for the "Blue" link.
 
 $(document).ready(function(){
-  $('.blue-trigger').on("hover", function(){
+  $('.blue-trigger').on("hover", function(event){
+  	// Prevent default browser action for a hover click
+  	 event.preventDefault()
   	$("a").not(this).addClass("blue");
   });
 });
 
+
+// Make a new link called "Sidebar" in the nav that hides and shows the sidebar 3 seconds after you click on it. 
+
+$(document).ready(function(){
+	var $sidebar = $('#sidebar');
+    
+    var hideSidebar = function(){
+        $sidebar.addClass('hide');
+    }
+    
+    var showSidebar = function(){
+        $sidebar.removeClass('hide');
+    }
+
+     $('.sidebar-toggle-trigger').on('click', function(event){
+        // Prevent default browser action for a link click
+        event.preventDefault()
+        
+        // Toggle action
+        if ($sidebar.hasClass('hide')) {
+            window.setTimeout(showSidebar, 3000);
+        } else {
+            window.setTimeout(hideSidebar, 3000);
+        }       
+    })
+
+});
